6513c3cfbfbb35b173ea91c7947c17881553f92f
lapeko <lapeko88@gmail.com>
2024-09-25 08:57:06 +0200

    fix migrations. do not use enum

--- storage/migrations/000001_init_schema.up.sql
+++ storage/migrations/000001_init_schema.up.sql
@@ -1,27 +1,19 @@
-DO $$
-BEGIN
-    IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'currency') THEN
-        CREATE TYPE "currency" AS ENUM ('USD', 'EURO', 'PLN');
-    END IF;
-END $$;
-
-
-CREATE TABLE IF NOT EXISTS "account" (
+CREATE TABLE IF NOT EXISTS "accounts" (
     "id" bigserial PRIMARY KEY,
     "owner" varchar NOT NULL,
-    "currency" currency NOT NULL,
+    "currency" char(3) NOT NULL,
     "balance" bigint NOT NULL,
     "created_at" timestamptz NOT NULL DEFAULT (now())
 );
 
-CREATE TABLE IF NOT EXISTS "entry" (
+CREATE TABLE IF NOT EXISTS "entries" (
     "id" bigserial PRIMARY KEY,
     "account_id" bigint NOT NULL,
     "amount" bigint NOT NULL,
     "created_at" timestamptz NOT NULL DEFAULT (now())
 );
 
-CREATE TABLE IF NOT EXISTS "transfer" (
+CREATE TABLE IF NOT EXISTS "transfers" (
     "id" bigserial PRIMARY KEY,
     "account_from" bigint NOT NULL,
     "account_to" bigint NOT NULL,
@@ -29,33 +21,33 @@ CREATE TABLE IF NOT EXISTS "transfer" (
     "created_at" timestamptz NOT NULL DEFAULT (now())
 );
 
-CREATE INDEX IF NOT EXISTS "idx_account_owner " ON "account" ("owner");
-CREATE INDEX IF NOT EXISTS idx_entry_account_id ON "entry" ("account_id");
-CREATE INDEX IF NOT EXISTS idx_transfer_account_from ON "transfer" ("account_from");
-CREATE INDEX IF NOT EXISTS idx_transfer_account_to ON "transfer" ("account_to");
-CREATE INDEX IF NOT EXISTS idx_transfer_account_from_to ON "transfer" ("account_from", "account_to");
+CREATE INDEX IF NOT EXISTS "idx_accounts_owner" ON "accounts" ("owner");
+CREATE INDEX IF NOT EXISTS idx_entries_account_id ON "entries" ("account_id");
+CREATE INDEX IF NOT EXISTS idx_transfers_account_from ON "transfers" ("account_from");
+CREATE INDEX IF NOT EXISTS idx_transfers_account_to ON "transfers" ("account_to");
+CREATE INDEX IF NOT EXISTS idx_transfers_account_from_to ON "transfers" ("account_from", "account_to");
 
 
-COMMENT ON COLUMN "account"."balance" IS 'should be positive';
-COMMENT ON COLUMN "transfer"."amount" IS 'should be positive';
+COMMENT ON COLUMN "accounts"."balance" IS 'should be positive';
+COMMENT ON COLUMN "transfers"."amount" IS 'should be positive';
 
 DO $$
 BEGIN
-    IF NOT EXISTS (SELECT 1 FROM pg_constraint WHERE conname = 'entry_account_id_fkey') THEN
-        ALTER TABLE "entry" ADD CONSTRAINT entry_account_id_fkey FOREIGN KEY ("account_id") REFERENCES "account" ("id");
+    IF NOT EXISTS (SELECT 1 FROM pg_constraint WHERE conname = 'entries_account_id_fkey') THEN
+        ALTER TABLE "entries" ADD CONSTRAINT entries_account_id_fkey FOREIGN KEY ("account_id") REFERENCES "accounts" ("id");
     END IF;
 END $$;
 
 DO $$
 BEGIN
-    IF NOT EXISTS (SELECT 1 FROM pg_constraint WHERE conname = 'transfer_account_from_fkey') THEN
-        ALTER TABLE "transfer" ADD CONSTRAINT transfer_account_from_fkey FOREIGN KEY ("account_from") REFERENCES "account" ("id");
+    IF NOT EXISTS (SELECT 1 FROM pg_constraint WHERE conname = 'transfers_account_from_fkey') THEN
+        ALTER TABLE "transfers" ADD CONSTRAINT transfers_account_from_fkey FOREIGN KEY ("account_from") REFERENCES "accounts" ("id");
     END IF;
 END $$;
 
 DO $$
 BEGIN
-    IF NOT EXISTS (SELECT 1 FROM pg_constraint WHERE conname = 'transfer_account_to_fkey') THEN
-        ALTER TABLE "transfer" ADD CONSTRAINT transfer_account_to_fkey FOREIGN KEY ("account_to") REFERENCES "account" ("id");
+    IF NOT EXISTS (SELECT 1 FROM pg_constraint WHERE conname = 'transfers_account_to_fkey') THEN
+        ALTER TABLE "transfers" ADD CONSTRAINT transfers_account_to_fkey FOREIGN KEY ("account_to") REFERENCES "accounts" ("id");
     END IF;
 END $$;
