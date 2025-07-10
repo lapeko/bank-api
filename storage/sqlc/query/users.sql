1ad256ba9282ff318a27b62f1f3f03b41c6e2ec5
lapeko <lapeko88@gmail.com>
2025-07-10 14:09:39 +0200

    start from scratch

--- storage/sqlc/query/users.sql
+++
@@ -1,8 +0,0 @@
--- name: GetUser :one
-SELECT * FROM users
-WHERE id = $1 LIMIT 1;
-
--- name: CreateUser :one
-INSERT INTO users (full_name, email, hashed_password)
-VALUES ($1, $2, $3)
-RETURNING *;
